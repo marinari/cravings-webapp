4bcdeea0e4a0a1a2e42c0663cdda2db14879a8c2
Arianna Marin <marin.arianna@gmail.com>
2016-05-31 18:35:48 -0700

    lng and lat of seattle added to map

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
+          center: {lat: 47.611429, lng: -122.334493},
+          zoom: 15
         });
       }
 /*
