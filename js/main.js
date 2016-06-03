b5aae0f19cab9f5e860a98e27de30b10b38555da
Arianna Marin <marin.arianna@gmail.com>
2016-06-02 22:58:41 -0700

    map works, button added to test functions

--- js/main.js
+++ js/main.js
@@ -1,10 +1,42 @@
-var map;
-      function initMap() {
+var seattle;
+
+function initMap() {
+        var map;
+        seattle = new google.maps.LatLng (47.611429,-122.334493);
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 47.611429, lng: -122.334493},
+          center: seattle,
           zoom: 15
         });
+        var defaultBounds = new google.maps.LatLngBounds(
+          new google.maps.LatLng(-33.8902, 151.1759),
+          new google.maps.LatLng(-33.8474, 151.2631));
+
+        var input = document.getElementById('mapsearch');
+
+        var searchBox = new google.maps.places.SearchBox(input, {
+          bounds: defaultBounds
+        });
       }
+
+      function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            var place = results[i];
+            console.log(place);
+          }
+        }
+      }
+
+function cabinet(){
+  var request = {
+      location: seattle,
+      radius: '500',
+      query: 'restaurant'
+    };
+  service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+  console.log('this button is now working');
+}
 /*
 var map;
 var infowindow;
